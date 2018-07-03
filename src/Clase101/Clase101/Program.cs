04b44d488aa9b726e7927bc16d1b4287c120fcab
08blackknight <40481797+08blackknight@users.noreply.github.com>
2018-07-02 21:07:30 -0300

    Base del programa

---
+++ src/Clase101/Clase101/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerouno;
+            int numerodos;
+            int suma;
+            Console.WriteLine("Escriba un numero:");
+            numerouno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba otro numero:");
+            numerodos = Convert.ToInt32(Console.ReadLine());
+            suma = numerouno + numerodos;
+            Console.WriteLine("El resultado es: " + suma);
+            Console.ReadKey();
+        }
+    }
+}
