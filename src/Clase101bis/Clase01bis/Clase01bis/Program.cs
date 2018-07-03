8c174f8c46d3e8975f4273b02f0629f37e9c4c88
08blackknight <40481797+08blackknight@users.noreply.github.com>
2018-07-02 21:19:55 -0300

    Variable String

---
+++ src/Clase101bis/Clase01bis/Clase01bis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase01bis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string nombre = "Jose Antonio";
+            string apellido = "Serrano Mesa";
+            Console.WriteLine(nombre + " " + apellido);
+            Console.ReadKey();
+            
+
+        }
+    }
+}
