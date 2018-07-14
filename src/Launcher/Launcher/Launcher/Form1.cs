e68b381aa045e20c0d9ebf08a02d5a3ed3bb3e9d
Nico <bttf-isback@hotmail.com>
2018-07-14 02:37:33 -0300

    Launcher BOT

---
+++ src/Launcher/Launcher/Launcher/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            //this.ShowDialog.Hide();
+            // System.Diagnostics.Process.Start("notepad.exe", "");
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "node";
+            cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            cmd.StartInfo.Arguments = "index.dfs";
+            cmd.Start();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "taskkill";
+            cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            cmd.StartInfo.Arguments = "/f /im node.exe";
+            cmd.Start();
+
+
+        }
+    }
+}
